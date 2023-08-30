9b5bd4ad8c83e1f4662f06341fb2af6a7244b6c8
Danielle Ramirez <ramirezdanielle88@gmail.com>
2023-08-30 10:31:11 -0700

    updated localhost

--- server.py
+++ server.py
@@ -2,5 +2,5 @@ from flask_app import app
 from flask_app.controllers import users
 from flask_app.controllers import recipes
 if __name__=='__main__':
-    app.run(debug=True)
+ app.run(debug=True,host='localhost',port=5001)
 
